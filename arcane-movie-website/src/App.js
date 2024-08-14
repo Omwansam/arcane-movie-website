b0d2e098593935d2d6e3d0860afee98dcd829b14
IanDanCheruiyot <daniank08@gmail.com>
2024-08-14 04:42:51 +0300

    posters

--- arcane-movie-website/src/App.js
+++ arcane-movie-website/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './App.css';
+import Posters from './index';
 
 function App() {
   const movies = [
     {
-      title: '',
-      imageUrl: '',
+      title: 'Fellowship Of The Ring',
+      imageUrl: 'https://i.pinimg.com/564x/66/4f/b3/664fb36db5d95bb35a826328038d98c8.jpg',
     },
     {
-      title: '',
-      imageUrl: '',
+      title: 'Baby Driver',
+      imageUrl: 'https://i.pinimg.com/564x/1b/d4/fa/1bd4fa8d38095b735c3f65f4553fe434.jpg',
     },
     {
-      title: '',
-      imageUrl: '',
+      title: 'The Godfather',
+      imageUrl: 'https://i.pinimg.com/564x/9b/bc/ae/9bbcae6cb69bee8f0cb90f5b3d3f35a0.jpg',
+    },
+    {
+      title: 'Kimetsu No Yaiba',
+      imageUrl: 'https://i.pinimg.com/564x/fc/49/93/fc49938607e16ea63fcefbe68ebc0a53.jpg',
+    },
+    {
+      title: 'O brother Where art thou?',
+      imageUrl: 'https://i.pinimg.com/564x/f2/fa/9e/f2fa9e08689c1c23c50b7404576cfb11.jpg',
     },
   ];
 
@@ -22,7 +31,7 @@ function App() {
       <h1>Movie</h1>
       <div className="poster-container">
         {movies.map((movie, index) => (
-          <MoviePoster key={index} title={movie.title} imageUrl={movie.imageUrl} />
+          <Posters key={index} title={movie.title} imageUrl={movie.imageUrl} />
         ))}
       </div>
     </div>
--- arcane-movie-website/src/index.js
+++ arcane-movie-website/src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import './index.css';
 
+function Posters({ title, imageUrl }) {
+  return (
+    <div className="movie-poster">
+      <img src={imageUrl} alt={`${title} poster`} />
+      <h2>{title}</h2>
+    </div>
+  );
+}
+
+export default Posters;
